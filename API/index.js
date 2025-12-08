50e9a037efd1bfa0731ae0200189931ed632aee1
ridho <alhusainridho@gmail.com>
2025-12-09 00:42:31 +0700

    baru

--- API/index.js
+++ API/index.js
@@ -3,16 +3,20 @@ const path = require("path");
 
 module.exports = (req, res) => {
   try {
-    const dataPath = path.join(__dirname, "..", "data", "vendorC.json");
-    const raw = fs.readFileSync(dataPath, "utf8");
+    // Path ke JSON
+    const jsonPath = path.join(__dirname, "..", "DATA", "vendorC.json");
+
+    // Baca isi file
+    const raw = fs.readFileSync(jsonPath, "utf8");
     const data = JSON.parse(raw);
 
-    res.status(200).json(data); // RAW sesuai PDF
-    
+    // Vendor C HARUS mengirim RAW (TANPA NORMALISASI)
+    res.status(200).json(data);
+
   } catch (error) {
-    res.status(500).json({ 
-      error: "Gagal membaca data Vendor C",
-      detail: error.message 
+    res.status(500).json({
+      error: "Gagal membaca data vendor C",
+      message: error.message
     });
   }
 };
